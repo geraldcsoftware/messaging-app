35df8b54205d46e64ff460183fac3a422277e989
Gerald Chifanzwa <g.chifanzwa@theahoyapp.com>
2022-05-19 15:37:34 +0400

    List campaigns endpoint and unit test

---
+++ MessagingApp.Api/Endpoints/ListCampaignsEndpoint.cs
@@ -0,0 +1,69 @@
+﻿using FastEndpoints;
+using MapsterMapper;
+using MessagingApp.Api.Configuration;
+using MessagingApp.Api.ViewModels;
+using MessagingApp.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace MessagingApp.Api.Endpoints;
+
+public class ListCampaignsEndpoint : EndpointWithoutRequest<PagedCollection<CampaignViewModel>>
+{
+    private readonly AppDbContext _dbContext;
+    private readonly IMapper _mapper;
+    private readonly ILogger<ListCampaignsEndpoint> _logger;
+    private readonly IOptions<PagingOptions> _pagingOptions;
+
+    public ListCampaignsEndpoint(AppDbContext dbContext,
+                                 IMapper mapper,
+                                 ILogger<ListCampaignsEndpoint> logger,
+                                 IOptions<PagingOptions> pagingOptions)
+    {
+        _dbContext = dbContext;
+        _mapper = mapper;
+        _logger = logger;
+        _pagingOptions = pagingOptions;
+    }
+
+    public override void Configure()
+    {
+        Get("clients/{clientId}/campaigns");
+    }
+
+    public override async Task<PagedCollection<CampaignViewModel>> ExecuteAsync(CancellationToken ct)
+    {
+        try
+        {
+            var clientId = Route<string>("clientId");
+
+            var page = Query<int?>("page", false)         ?? 1;
+            var pageSize = Query<int?>("pageSize", false) ?? _pagingOptions.Value.DefaultPageSize;
+
+            var query = _dbContext.Campaigns.Where(c => c.ClientId == clientId);
+
+            var count = await query.CountAsync(ct);
+            var totalPages = count == 0 ? 0 : (int)Math.Ceiling((double)count / pageSize);
+            var items = await query.AsNoTracking()
+                                   .OrderBy(c => c.Created)
+                                   .ThenBy(c => c.Name)
+                                   .Skip((page - 1) * pageSize)
+                                   .Take(pageSize)
+                                   .ToListAsync(ct);
+
+            return new()
+            {
+                Page = page,
+                PageSize = pageSize,
+                Items = _mapper.Map<List<CampaignViewModel>>(items),
+                TotalItems = count,
+                TotalPages = totalPages
+            };
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "Error getting campaigns");
+            throw;
+        }
+    }
+}
---
+++ MessagingApp.Tests/Endpoints/ListCampaignsEndpointTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Net;
+using System.Text.Json;
+using Bogus;
+using FluentAssertions;
+using MessagingApp.Infrastructure;
+using MessagingApp.Infrastructure.Models;
+using MessagingApp.Tests.Utilities;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MessagingApp.Tests.Endpoints;
+
+public class ListCampaignsEndpointTests : IClassFixture<ApplicationFactory>
+{
+    private readonly ApplicationFactory _factory;
+
+
+    public ListCampaignsEndpointTests(ApplicationFactory factory)
+    {
+        _factory = factory;
+    }
+
+
+    [Fact]
+    public async Task ListCampaigns_WhenCampaignsExists_ReturnsOkWithCampaignDetails()
+    {
+        // Arrange
+        var clientId = Guid.NewGuid().ToString();
+
+        var factory = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                using var scope = services.BuildServiceProvider().CreateScope();
+                var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                context.Database.EnsureCreated();
+                var client = new Faker<Client>()
+                            .RuleFor(x => x.Id, clientId)
+                            .RuleFor(x => x.Name, f => f.Person.Company.Name)
+                            .RuleFor(x => x.Email, f => f.Person.Email)
+                            .Generate();
+                var campaigns = new Faker<MessageCampaign>()
+                               .RuleFor(x => x.Id, f => f.Random.Guid().ToString())
+                               .RuleFor(x => x.Name, f => f.Lorem.Word())
+                               .RuleFor(x => x.Template, f => f.Lorem.Sentence())
+                               .RuleFor(x => x.Created, f => f.Date.Recent())
+                               .RuleFor(x => x.ClientId, clientId)
+                               .Generate(50);
+
+                context.Clients.Add(client);
+                context.Campaigns.AddRange(campaigns);
+                context.SaveChanges();
+            });
+        });
+
+        var client = factory.CreateClient();
+        var url = $"/api/clients/{clientId}/campaigns?page=1&pageSize=10";
+
+        // Act
+        var response = await client.GetAsync(url);
+        var responseContent = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        responseContent.Should().NotBeNullOrEmpty();
+        var responseJson = JsonDocument.Parse(responseContent);
+
+        responseJson.RootElement.GetProperty("totalItems").GetInt32().Should().Be(50);
+        responseJson.RootElement.GetProperty("items").GetArrayLength().Should().Be(10);
+    }
+}
