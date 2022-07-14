1a6355d25b03841d613181d9330a5deefe09bfcb
Gerald Chifanzwa <g.chifanzwa@theahoyapp.com>
2022-07-14 19:37:55 +0400

    Schedule campaign request validator

---
+++ MessagingApp.Api/Validators/CustomValidationRules.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+using FluentValidation;
+
+namespace MessagingApp.Api.Validators;
+
+public static partial class CustomValidationRules
+{
+    public static IRuleBuilderOptions<T, IReadOnlyCollection<string>> PhoneNumberList<T>(
+        this IRuleBuilder<T, IReadOnlyCollection<string>> ruleBuilder)
+    {
+        return ruleBuilder.Must((_, list, context) =>
+                           {
+                               var phoneNumberRegex = PhoneNumberRegex();
+                               foreach (var phoneNumber in list)
+                               {
+                                   if (phoneNumberRegex.IsMatch(phoneNumber))
+                                   {
+                                       continue;
+                                   }
+
+                                   context.MessageFormatter.AppendArgument("PhoneNumber", phoneNumber);
+                                   return false;
+                               }
+
+                               return true;
+                           })
+                          .WithMessage("`{PhoneNumber}` is not a valid phone number");
+    }
+
+    [RegexGenerator(@"^\+?\d{10,13}$", RegexOptions.Compiled)]
+    private static partial Regex PhoneNumberRegex();
+}
---
+++ MessagingApp.Api/Validators/ScheduleCampaignRequestValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using MessagingApp.Api.ViewModels;
+using MessagingApp.Scheduling;
+
+namespace MessagingApp.Api.Validators;
+
+public class ScheduleCampaignRequestValidator: AbstractValidator<ScheduleCampaignRequest>
+{
+    public ScheduleCampaignRequestValidator()
+    {
+        RuleFor(x => x.CampaignId).NotEmpty();
+        RuleFor(x => x.ScheduleType).NotEmpty();
+        RuleFor(x => x.ClientId).NotEmpty();
+        RuleFor(x => x.Customers).NotEmpty()
+                                 .Must(customers => customers.Count > 0)
+                                 .WithMessage("You must select at least one customer")
+                                 .PhoneNumberList();
+
+        When(x => x.ScheduleType == ScheduleTypes.Instant,
+            () =>
+                {
+                    // Validate instant schedules.
+                    RuleFor(x => x.Customers)
+                        .Must(val => val.Count < 10)
+                        .WithMessage("Cannot create an instant message for more than 1 customer");
+                });
+    }
+}
